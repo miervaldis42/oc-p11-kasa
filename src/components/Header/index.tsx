4e50d76c8541ed0f45e0b1248970ab96c611f1ad
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2023-07-05 07:04:03 +0900

    feat & style (Components) : Header Creation
    - Move `<Header>` component from the `/layouts` folder to the `/components` one
    - Delete `/layouts` folder as the layout code splitting is not relevant for this project
    - Implement `<Header>` component by using `react-router-dom` elements such as `<NavLink>` and the custom made `pathList`
    - Style the component using the classes and `active` built-in class for the `<NavLink>`
    - Make the logo redirect the user to the home page from anywhere in the website
    - Create a media query for the desktop version to stylish the header
    - Add global styling for lists & anchor tags

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,52 @@
+// Imports
+import { NavLink, Link, useLocation } from "react-router-dom";
+
+import pathList from "router/pathList";
+
+// Styles
+import styles from "./Header.module.css";
+import { ReactComponent as Logo } from "assets/logo.svg";
+
+// Component
 export default function Header() {
-  return <h1>Header</h1>;
+  // Check if the current page is the Home page
+  const { pathname } = useLocation();
+  const isHomePage = pathname === "/";
+
+  return (
+    <header className={styles.header}>
+      <Link
+        to={isHomePage ? "#" : pathList.home}
+        style={{ cursor: `${isHomePage ? "default" : "pointer"}` }}
+      >
+        <Logo className={styles.logo} />
+      </Link>
+
+      {/* Navbar */}
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to={pathList.home}
+              className={({ isActive }) =>
+                `${isActive ? styles.active : styles.navLink}`
+              }
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={pathList.about}
+              className={({ isActive }) =>
+                `${styles.navLink} ${isActive && styles.active}`
+              }
+            >
+              A Propos
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }

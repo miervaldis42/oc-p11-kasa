af197cd22288ddcce90f4b571b3cf220a31f033b
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2023-07-06 21:16:15 +0900

    feat & style (Home) : Hero Section Creation
    - Start formatting the layout of the Home page
    - Create a component for the Hero section
    - Stylish the created component

---
+++ src/components/Hero/HeroSection.tsx
@@ -0,0 +1,11 @@
+// Styles
+import styles from "./HeroSection.module.css";
+
+// Component
+export default function HeroSection() {
+  return (
+    <section className={styles.hero}>
+      <p>Chez vous, partout et ailleurs</p>
+    </section>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,14 @@
+// Imports
+import HeroSection from "components/Hero/HeroSection";
+
+// Styles
+import styles from "./Home.module.css";
+
+// Component
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <main className={styles.home}>
+      <HeroSection />
+    </main>
+  );
 }

220dd2b9f567bbe3a24a9fa11b9e79f00c717874
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2023-07-09 03:22:04 +0900

    feat & style (404) : 404 Page Creation
    - Add the content to the 404 Page component
    - Add stylings to the pages with CSS classes
    - Use media query to add styling for the desktop version

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,3 +1,23 @@
+// Styles
+import { Link } from "react-router-dom";
+
+import pathList from "router/pathList";
+
+// Styles
+import styles from "./Custom404.module.css";
+
+// Component
 export default function Custom404() {
-  return <p>404</p>;
+  return (
+    <div className={styles.errorPage}>
+      <>
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+      </>
+
+      <Link to={pathList.home} className={styles.hyperlink}>
+        Retourner sur la page d’accueil
+      </Link>
+    </div>
+  );
 }
